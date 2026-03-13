6888c5e4216727513cefe14b007b2fd030b2206b
Fankouzu <1046166@qq.com>
2026-03-13 21:27:17 +0800

    Add .sisyphus to .gitignore and update Character class in characters.py for enhanced functionality

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -13,14 +13,32 @@ from evennia.objects.objects import DefaultCharacter
 from .objects import ObjectParent
 
 
-class Character(ObjectParent, DefaultCharacter):
+# class Character(ObjectParent, DefaultCharacter):
+#     """
+#     The Character just re-implements some of the Object's methods and hooks
+#     to represent a Character entity in-game.
+
+#     See mygame/typeclasses/objects.py for a list of
+#     properties and methods available on all Object child classes like this.
+
+#     """
+
+#     pass
+"""
+Characters
+
+这是所有角色的底层躯壳蓝图。
+"""
+from evennia.contrib.tutorials.evadventure.characters import EvAdventureCharacter
+from evennia.contrib.tutorials.evadventure.combat_twitch import TwitchCombatCmdSet
+
+class Character(EvAdventureCharacter):
     """
-    The Character just re-implements some of the Object's methods and hooks
-    to represent a Character entity in-game.
-
-    See mygame/typeclasses/objects.py for a list of
-    properties and methods available on all Object child classes like this.
-
+    魔改版：继承官方高级躯壳，并强行植入战斗芯片！
     """
+    def at_object_creation(self):
+        # 先执行官方的初始化（生成血条、蓝条、属性等）
+        super().at_object_creation()
 
-    pass
+        # 【核心黑科技】：给所有新生儿的大脑里永久挂载即时战斗（Twitch Combat）指令包！
+        self.cmdset.add(TwitchCombatCmdSet, persistent=True)
